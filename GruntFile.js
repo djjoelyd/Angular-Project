d89d7cd3356feb268a87e18d921e2159f50d376b
Joel Delane <joeldelane@gmail.com>
2016-04-28 14:39:35 +0100

    Initial commit

---
+++ GruntFile.js
@@ -0,0 +1,153 @@
+(function() {
+    'use strict';
+}());
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        // Project configuration
+        pkg: grunt.file.readJSON('package.json'),
+
+        // Compile Sass
+        sass: {
+            options: {
+                style: 'compressed'
+            },
+            dist: {
+                files: {
+                    'dist/css/styles.min.css': 'dev/scss/styles.scss'
+                }
+            }
+        },
+
+        // Copy html files
+        copy: {
+            main: {
+                files: [{
+                    expand: true,
+                    cwd: 'dev',
+                    src: [
+                        '**/*.html',
+                        '**/*.php',
+                        'images/{,*/}*.{png,jpg,gif,svg}',
+                        'js/libs/*.js',
+                        'js/libs/*.js.map',
+                        'fonts/{,*/}*.{eot,svg,ttf,woff,woff2,otf}'
+                    ],
+                    dest: 'dist'
+                }]
+            }
+
+        },
+
+        concat: {
+            options: {},
+            dist: {
+                src: ['dev/js/*.js'],
+                dest: 'dist/js/main.js'
+            }
+        },
+
+        uglify: {
+            options: {
+                mangle: false
+            },
+            dist: {
+                files: {
+                    'dist/js/main.min.js': 'dist/js/main.js'
+                }
+            }
+        },
+
+        jshint: {
+            files: ['gruntfile.js', 'dev/js/*.js'],
+            options: {
+                globals: {
+                    jQuery: true,
+                    console: true,
+                    module: true
+                }
+            }
+        },
+
+        //Browersync
+        browserSync: {
+            default_options: {
+                bsFiles: {
+                    src: [
+                        "dist/css/*.css",
+                        "dist/js/*.js",
+                        "dist/*.html"
+                    ]
+                },
+                options: {
+                    watchTask: true,
+                    server: {
+                        baseDir: "./dist"
+                    }
+                }
+            }
+        },
+
+        // NG Annotate to Add, remove and rebuild AngularJS dependency injection annotations.
+
+        ngAnnotate: {
+            options: {
+                // Task-specific options go here. 
+            },
+            your_target: {
+                // Target-specific file lists and/or options go here. 
+            },
+        },
+
+        // Watch and build
+        watch: {
+
+            sass: {
+                files: 'dev/scss/*.scss',
+                tasks: ['sass'],
+                options: {
+                    livereload: true
+                }
+            },
+
+            html: {
+                files: ['dev/*.html'],
+                tasks: ['copy'],
+                options: {
+                    livereload: true
+                }
+            },
+
+            images: {
+                files: ['dist/images/{,*/}*.{png,jpg,gif,svg}'],
+                options: {
+                    livereload: true
+                }
+            },
+
+            js: {
+                files: ['dist/js/**/*.js'],
+                options: {
+                    livereload: true
+                }
+            }
+        }
+
+    });
+
+    // Load dependencies
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-ng-annotate');
+
+    // Run tasks
+    grunt.registerTask('default', ['sass', 'concat', 'uglify', 'jshint', 'copy', 'browserSync', 'watch', ]);
+
+};
---
+++ Readme.md
@@ -0,0 +1,36 @@
+# Angular Test Site
+
+## Overview
+
+This is a test site setup using Grunt. Built by [Joel Delane](http://joeldelane.com).
+
+https://github.com/djjoelyd/Angular-Project
+
+### Notes on the codebase
+
+The code is for user testing only, and is not intended for production use. 
+
+### Libraries and Tools
+
+TBA
+
+## Structure
+
+### Files
+
+TBA
+
+## Installation, Build and Deployment
+
+Clone the repo:
+
+	$ git clone https://github.com/djjoelyd/Angular-Project.git
+	$ git checkout master
+
+Install the dependencies
+
+	$ npm install
+
+Load the index.html file in your browser
+
+	$ grunt
